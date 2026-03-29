b7b8de4762430ef3ad4b2e84a7b73932b1c2bc13
Sage-senpai <157594837+Sage-senpai@users.noreply.github.com>
2026-03-29 19:37:01 +0100

    feat: implement card glassmorphism borders

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Nav from "./components/Nav";
+import Nav from "./components/nav";
 
 const page = () => {
   return (
-    <main>
+    <main className="border border-white/10 rounded-2xl m-4 min-h-[calc(100vh-2rem)]">
       <Nav />
     </main>
   );
