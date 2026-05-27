0132914f69c0977bcb755f2b616187c39349f983
mekeke <abdullahimekeke@gmail.com>
2026-05-26 17:38:00 -0700

    fix  package.json

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-const compression = require('shrink-ray-current'); // For Brotli support
 const { WebSocketServer } = require('ws');
 
 const dev = process.env.NODE_ENV !== 'production';
@@ -14,26 +13,9 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = createServer((req, res) => {
-    // Apply compression middleware
-    // shrink-ray-current will automatically detect Accept-Encoding and apply Brotli/Gzip
-    compression({
-      // Optional: Configure options for shrink-ray-current
-      // For example, to only compress certain types:
-      // filter: (req, res) => {
-      //   return /json|text|javascript|css|image\/svg\+xml/.test(res.getHeader('Content-Type'));
-      // },
-      // brotli: {
-      //   quality: 11, // Brotli compression quality (0-11)
-      // },
-      // gzip: {
-      //   level: 9, // Gzip compression level (0-9)
-      // }
-    })(req, res, () => {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true);
-      handle(req, res, parsedUrl);
-    });
+    // Use the default Next.js request handler.
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
   });
 
   // Set up WebSocket server
